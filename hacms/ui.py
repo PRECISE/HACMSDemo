f0b52356f5334430a2da1f1441c13b17e2149c30
Peter Gebhard <pgeb@seas.upenn.edu>
2013-06-25 16:43:48 -0400

    Updated generated ui.py.

--- hacms/ui.py
+++ hacms/ui.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'mainwindow.ui'
 #
-# Created: Mon Jun 24 21:07:12 2013
+# Created: Tue Jun 25 16:40:02 2013
 #      by: pyside-uic 0.2.13 running on PySide 1.1.2
 #
 # WARNING! All changes made in this file will be lost!
@@ -12,7 +12,7 @@ from PySide import QtCore, QtGui
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(445, 363)
+        MainWindow.resize(481, 380)
         self.centralWidget = QtGui.QWidget(MainWindow)
         self.centralWidget.setObjectName("centralWidget")
         self.startRobotButton = QtGui.QPushButton(self.centralWidget)
@@ -22,22 +22,26 @@ class Ui_MainWindow(object):
         self.startRCButton.setGeometry(QtCore.QRect(20, 60, 201, 41))
         self.startRCButton.setObjectName("startRCButton")
         self.attackButton = QtGui.QPushButton(self.centralWidget)
-        self.attackButton.setGeometry(QtCore.QRect(220, 20, 171, 81))
+        self.attackButton.setGeometry(QtCore.QRect(240, 20, 171, 81))
         self.attackButton.setObjectName("attackButton")
         self.actualSpeedLCD = QtGui.QLCDNumber(self.centralWidget)
         self.actualSpeedLCD.setGeometry(QtCore.QRect(40, 220, 171, 71))
         self.actualSpeedLCD.setObjectName("actualSpeedLCD")
         self.expectedLabel = QtGui.QLabel(self.centralWidget)
-        self.expectedLabel.setGeometry(QtCore.QRect(250, 130, 171, 51))
+        self.expectedLabel.setGeometry(QtCore.QRect(250, 130, 161, 51))
         font = QtGui.QFont()
         font.setPointSize(36)
         self.expectedLabel.setFont(font)
+        self.expectedLabel.setAutoFillBackground(False)
+        self.expectedLabel.setStyleSheet("background-color: green;")
         self.expectedLabel.setObjectName("expectedLabel")
         self.actualLabel = QtGui.QLabel(self.centralWidget)
-        self.actualLabel.setGeometry(QtCore.QRect(250, 230, 121, 51))
+        self.actualLabel.setGeometry(QtCore.QRect(250, 230, 161, 51))
         font = QtGui.QFont()
         font.setPointSize(36)
         self.actualLabel.setFont(font)
+        self.actualLabel.setAutoFillBackground(False)
+        self.actualLabel.setStyleSheet("background-color: green;")
         self.actualLabel.setObjectName("actualLabel")
         self.desiredSpeedLabel = QtGui.QLabel(self.centralWidget)
         self.desiredSpeedLabel.setGeometry(QtCore.QRect(70, 120, 101, 16))
@@ -50,7 +54,7 @@ class Ui_MainWindow(object):
         self.actualSpeedLabel.setObjectName("actualSpeedLabel")
         MainWindow.setCentralWidget(self.centralWidget)
         self.menuBar = QtGui.QMenuBar()
-        self.menuBar.setGeometry(QtCore.QRect(0, 0, 445, 22))
+        self.menuBar.setGeometry(QtCore.QRect(0, 0, 481, 22))
         self.menuBar.setObjectName("menuBar")
         self.menuFile = QtGui.QMenu(self.menuBar)
         self.menuFile.setObjectName("menuFile")
