e985fe49b1d7136ef888879239bb276ef7f6045c
Peter Gebhard <pgeb@seas.upenn.edu>
2013-07-09 16:08:30 -0400

    Added another ROS Subscriber to watch GPS Velocity.
    Added 'import' statements for matplotlib and future plotting features.

--- hacms/demo.py
+++ hacms/demo.py
@@ -3,7 +3,13 @@ import rospy
 from std_msgs.msg import String
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import Imu
+from nav_msgs.msg import Odometry
 from PySide import QtGui
+import matplotlib
+matplotlib.use('Qt4Agg')
+import pylab
+from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
 
 # HACMS Python modules
 from remote import Remote
@@ -17,73 +23,83 @@ class HACMSDemoWindow(QtGui.QMainWindow):
         self.ui.actionAbout.triggered.connect(self.about)
         self.ui.actionQuit.triggered.connect(self.close)
         self.ui.landsharkButton.toggled.connect(self.landshark)
+        self.ui.ccButton.toggled.connect(self.cc)
         self.ui.rcButton.toggled.connect(self.rc)
         self.ui.attackButton.toggled.connect(self.attack)
+        #self.ui.setSpeedButton.triggered.connect(self.setLandsharkSpeed)
         self.remote = Remote(self.ui.console)
-    
+
     def about(self):
         QtGui.QMessageBox.about(self, "About HACMS Demo",
                 "The <b>HACMS Demo</b> application displays the current ROS telemetry "
                 "information.")
-                
+
     def landshark(self, checked):
         if checked:
             res = self.remote.startLandshark()
-            #self.hacms_listener()
+            #self.landshark_listener()
         else:
             self.rc(False)
             self.attack(False)
             res = self.remote.stopLandshark()
-            
+
         self.ui.landsharkButton.setChecked(res)
         
+    def cc(self, checked):
+        #self.ui.ccButton.setChecked(self.remote.startCC() if checked else self.remote.stopCC())
+        return
+
     def rc(self, checked):
-        self.ui.rcButton.setChecked(self.hacms_listener() if checked else False)
+        self.ui.rcButton.setChecked(self.landshark_listener() if checked else False)
         #self.ui.rcButton.setChecked(self.remote.startRC() if checked else self.remote.stopRC())
-        
+
     def attack(self, checked):
         self.ui.attackButton.setChecked(self.remote.startAttack() if checked else self.remote.stopAttack())
-    
+
     def getWidgetColor(self, widget):
         style = widget.styleSheet()
         if "background-color: green;" in style:
             return "green"
         if "background-color: red;" in style:
             return "red"
-    
+
     def toggleWidgetColor(self, widget):
         style = widget.styleSheet()
         if self.getWidgetColor(widget) is "green":
             widget.setStyleSheet(string.replace(style, "background-color: green;", "background-color: red;"))
         elif self.getWidgetColor(widget) is "red":
             widget.setStyleSheet(string.replace(style, "background-color: red;", "background-color: green;"))
-        
-    def updateActualSpeedLCD(self, msg):
-        self.ui.actualSpeedLCD.display(msg.linear_acceleration.x)
-        #self.ui.console.appendPlainText('*** Actual Speed: ' + str(twistMsg.linear.x))
-        
-    def rosTest(self, data):
-        rospy.loginfo(rospy.get_caller_id()+"I heard %s",data.data)
-        self.ui.console.appendPlainText('*** ROS TEST: ' + data.data)
 
-    def hacms_listener(self):
+    def updateActualSpeedLCD(self, msg):
+        self.ui.actualSpeedLCD.display(msg.twist.twist.linear.x)
+        
+    def updateEstimatedSpeedLCD(self, msg):
+        self.ui.estimatedSpeedLCD.display(msg.twist.linear.x)
+
+    def landshark_listener(self):
         # Initialize ROS node
         rospy.init_node('demo_listener', anonymous=True)
 
         # Subscribe to HACMS Demo topics
-        #rospy.Subscriber("demo_ui", String, self.rosTest)
-        #rospy.Subscriber("/landshark/odom", Twist, self.updateActualSpeedLCD)
-        rospy.Subscriber("/landshark/imu", Imu, self.updateActualSpeedLCD)
-        
+        rospy.Subscriber("/landshark/odom", Odometry, self.updateActualSpeedLCD)
+        rospy.Subscriber("/landshark/gps_velocity", TwistStamped, self.updateEstimatedSpeedLCD)
+
         return True
-    
+
+    def landshark_desired_speed_publisher(self):
+        pub = rospy.Publisher('demo_desired_speed', String)
+        r = rospy.Rate(5) # 5hz
+        while not rospy.is_shutdown():
+            pub.publish(str)
+            r.sleep()
+
 
 def main():
     app = QtGui.QApplication(sys.argv)
     h = HACMSDemoWindow()
     h.show()
     sys.exit(app.exec_())
-    
+
 if __name__ == "__main__":
     main()
-    
+
