b13893f65459a69bec1fbbb4428cf5ea4dd30d75
Peter Gebhard <pgeb@seas.upenn.edu>
2013-08-05 17:25:30 -0400

    Changed About dialog display method.

--- hacms/hacms.py
+++ hacms/hacms.py
@@ -177,7 +177,7 @@ class HACMSWindow(QMainWindow):
         self.waypointModel.setStringList(self.waypointList)
 
     def about(self):
-        ui.about_ui.Ui_Dialog().open()
+        ui.about_ui.Ui_Dialog().exec_()
                 
     def fileQuit(self):
         if self.ui.landsharkButton.isChecked():
