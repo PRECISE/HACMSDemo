0e590000038f562133b89bf475c5634006bead4e
Peter Gebhard <pgeb@seas.upenn.edu>
2013-12-12 12:50:15 -0500

    Removed dead comment line.

--- hacms/demo.py
+++ hacms/demo.py
@@ -2,10 +2,9 @@
 
 import hacms
 import ui.demo_ui
-     
+
 class HACMSDemoWindow(hacms.HACMSWindow):
     def __init__(self):
         super(HACMSDemoWindow, self).__init__()
         self.ui = ui.demo_ui.Ui_MainWindow()
         self.init_window()
-        #self.widgets.append
